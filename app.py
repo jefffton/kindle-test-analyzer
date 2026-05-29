c9a6d08545488a8e1667b5690f7a969f75f8dc27
jefffton <jefffton@amazon.com>
2026-05-29 06:06:23 +0000

    Add Render deployment config and dynamic port

--- app.py
+++ app.py
@@ -322,4 +322,5 @@ def get_result(job_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
